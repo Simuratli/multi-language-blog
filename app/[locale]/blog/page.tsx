e066d76cd6dbcc72e79a2a937fe73847d38c069b
Simuratli <simuratli@gmail.com>
2025-01-30 17:40:25 +0400

    posts

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,18 +1,15 @@
-import Card from "@/components/card/card";
+// import Card from "@/components/card/card";
 import Paging from "@/components/paging/paging";
-import { CARD_SIZE_ENUM } from "@/types/enums";
+// import { CARD_SIZE_ENUM } from "@/types/enums";
 import React from "react";
 
 const BlogPage = () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 my-10">
-        <Card
+        {/* <Card
           size={CARD_SIZE_ENUM.MEDIUM}
-          date="2021-09-09"
-          id="123"
-          image="https://miro.medium.com/v2/resize:fit:720/format:webp/1*liYz-YLEZY9SYvesH4OUlg.png"
-          title="Web Səhifələr Necə Render Olunur: Client-Side Render (CSR), Server-Side Render(SSR) və Incremental Static Regeneration(İSR) İzahı"
+          post={}
         />
         <Card
           size={CARD_SIZE_ENUM.MEDIUM}
@@ -34,7 +31,7 @@ const BlogPage = () => {
           id="123"
           image="https://miro.medium.com/v2/resize:fit:720/format:webp/1*liYz-YLEZY9SYvesH4OUlg.png"
           title="Web Səhifələr Necə Render Olunur: Client-Side Render (CSR), Server-Side Render(SSR) və Incremental Static Regeneration(İSR) İzahı"
-        />
+        /> */}
       </div>
       <Paging />
     </>
--- app/[locale]/categories/[categoryId]/page.tsx
+++ app/[locale]/categories/[categoryId]/page.tsx
@@ -1,13 +1,13 @@
-import Card from "@/components/card/card";
+// import Card from "@/components/card/card";
 import Paging from "@/components/paging/paging";
-import { CARD_SIZE_ENUM } from "@/types/enums";
+// import { CARD_SIZE_ENUM } from "@/types/enums";
 import React from "react";
 
 const CategoryId = () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 my-10">
-        <Card
+        {/* <Card
           size={CARD_SIZE_ENUM.MEDIUM}
           date="2021-09-09"
           id="123"
@@ -34,7 +34,7 @@ const CategoryId = () => {
           id="123"
           image="https://miro.medium.com/v2/resize:fit:720/format:webp/1*liYz-YLEZY9SYvesH4OUlg.png"
           title="Web Səhifələr Necə Render Olunur: Client-Side Render (CSR), Server-Side Render(SSR) və Incremental Static Regeneration(İSR) İzahı"
-        />
+        /> */}
       </div>
       <Paging />
     </>
